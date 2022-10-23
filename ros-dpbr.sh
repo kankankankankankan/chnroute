6e59061e3d06c985fb3ff3f07edf10ed133203e1
kankankankankankan <vip029@me.com>
2022-10-23 10:41:53 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,7 +3,7 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O CN.txt https://ispip.clang.cn/all_cn.txt
 
 {
 echo "/ip firewall address-list"
