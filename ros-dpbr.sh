f07ed7160d5ec3b4bed502f71e11c9de8e2d1b62
kankankankankankan <vip029@me.com>
2023-02-16 15:49:51 +0800

    Update

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -8,5 +8,16 @@ sed -i 's/list=CN/list=CN comment=AS4809/g' CN.txt
 
 cp CN.txt ../CN.rsc
 
+cp ../gfwlist2dnsmasq.sh ./
+chmod +x gfwlist2dnsmasq.sh
+sh gfwlist2dnsmasq.sh -l -o tmp
+sed -i 's/\./\\\\./g' tmp
+sed -i 's/$/\\$" } on-error={}/g' tmp
+sed -i 's/^/:do { add forward-to=198.18.0.2 type=FWD regexp=".*/g' tmp
+sed -i '1s/^/\/ip dns static\n/' tmp
+sed -i '1s/^/\/ip dns static remove [\/ip dns static find forward-to=198.18.0.2 ]\n/' tmp
+sed -i -e '$a\/ip dns cache flush' tmp
+cp tmp ../GFW.rsc
+
 cd ..
 rm -rf ./pbr
