27ef84c69a9625c40374178edd7492705f00d356
kankankankankankan <vip029@me.com>
2023-03-05 12:04:36 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,10 +3,19 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -O CN.txt http://www.iwik.org/ipcountry/mikrotik/CN
-sed -i 's/list=CN/list=CN comment=AS4809/g' CN.txt
+#wget --no-check-certificate -O CN.txt http://www.iwik.org/ipcountry/mikrotik/CN
+#sed -i 's/list=CN/list=CN comment=AS4809/g' CN.txt
+#cp CN.txt ../CN.rsc
 
-cp CN.txt ../CN.rsc
+# AS4809 BGP
+wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+{
+echo "/ip firewall address-list"
+for net in $(cat CN.txt) ; do
+  echo "add list=CN address=$net comment=AS4809"
+done
+} > ../CN.rsc
+
 
 cp ../gfwlist2dnsmasq.sh ./
 chmod +x gfwlist2dnsmasq.sh
