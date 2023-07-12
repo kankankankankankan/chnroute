6faebc0b39c76e6ebdb1874061cf7b0c9f8472fb
kankankankankankan <vip029@me.com>
2023-07-12 12:08:38 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -10,8 +10,6 @@ cd ./pbr
 # AS4809 BGP & Exclude-CN-LIST
 wget --no-check-certificate -c -O CN.txt https://ispip.clang.cn/all_cn_cidr.txt
 cp ../exclude_cn_list.txt ./
-cp ../gfwlist2dnsmasq.sh ./
-chmod +x gfwlist2dnsmasq.sh
 {
   echo "/log info \"Loading CN ipv4 address list\""
   echo "/ip firewall address-list"
@@ -24,20 +22,18 @@ chmod +x gfwlist2dnsmasq.sh
 } > ../CN.rsc
 
 
-
-echo "Executing GFW-LIST code..."
-sh gfwlist2dnsmasq.sh -l -o tmp
+cp ../gfwlist2dnsmasq.sh ./
+chmod +x gfwlist2dnsmasq.sh
+sh gfwlist2dnsmasq.sh -l -o tmp.txt
 {
   echo ":global dnsserver"
   echo "/ip dns static remove [/ip dns static find forward-to=\$dnsserver ]"
   echo "/ip dns static"
-  for net in $(cat tmp) ; do
+  for net in $(cat tmp.txt) ; do
     echo ":do { add forward-to=\$dnsserver type=FWD address-list=GFW-LIST match-subdomain=yes name=$net } on-error={}"
   done
 } > ../GFW-LIST-V7.rsc
 echo "GFW-LIST code executed successfully!"
-
-
 
 cd ..
 rm -rf ./pbr
